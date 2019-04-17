71eed830df6196302866810450b1b37f96e24a08
irenedemas <irenedemasc@gmail.com>
2019-04-17 10:38:32 +0200

    config updated

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `QWORTY`,
+    description: `Quote Keyboard`,
+    author: `@irenedemas`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
