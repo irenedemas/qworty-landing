3e963c2d8d01577a465f2da345b4d270371a0367
irenedemas <irenedemasc@gmail.com>
2019-05-04 12:04:21 +0200

    cache busting changed

--- gatsby-config.js
+++ gatsby-config.js
@@ -18,13 +18,14 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `QWORTY Quote Keyboard`,
+        short_name: `QWORTY`,
         start_url: `/`,
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/qworty-icon.png`,
+        icon: `src/images/qworty-icon.png`, // This path is relative to the root of the site.
+        cache_busting_mode: `name`,
       },
 
       resolve: `gatsby-plugin-google-analytics`,
